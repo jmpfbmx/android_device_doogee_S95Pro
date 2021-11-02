7a0dba4741f83f6909340a2181afe44c20bbb037
jmpfbmx <jmpf.bmx@gmail.com>
2021-11-02 20:12:02 +0100

    S95Pro: Delete patches
     * We already have that patch at MTKOSP source
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ib4971432fae2dbc766a11e92f56137af88ba0b25

--- patches/apply.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-cd ../../../..
-cd system/tools/mkbootimg
-echo -e "${GREEN}We are gonna patch system/tools/mkbootimg${NC}"
-patch -p1 < ../../../device/doogee/S95Pro/patches/system/tools/mkbootimg/0001-Don-t-eliminate-meaningless-non-zero-values.patch
-cd ../../..
